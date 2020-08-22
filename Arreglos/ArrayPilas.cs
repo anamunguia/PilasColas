1d7a5fb70881121e1db3748a2b22a8776505e528
ana Ontiveros <karinaontiveros@hotmail.com>
2020-08-22 03:11:20 -0700

    Add project files.

---
+++ Arreglos/ArrayPilas.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos
+{
+    public class ArrayPilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+
+        public ArrayPilas(int tamanio)
+        {
+            this.array = new string[tamanio];
+            this.tope = 0;
+            this.max = array.Length - 1;
+        }
+        private bool validaVacio()
+        {
+            return (tope < 1);
+        }
+        private bool validaLleno()
+        {
+            return (tope > max);
+        }
+        public void Agregar(string dato)
+        {
+            if (validaLleno())
+            {
+                throw new Exception("arreglo lleno");
+            }
+            array[tope] = dato;
+            tope++;
+        }
+        public void Eliminar()
+        {
+            if (validaVacio())
+            {
+                throw new Exception("arreglo vacío");
+            }
+            tope--;
+            array[tope] = null;
+
+            string.IsNullOrEmpty("");
+        }
+        public string Imprimir()
+        {
+            string dato = string.Empty;
+            if (validaVacio())
+            {
+                return "arreglo vacio";
+            }
+            for (int i = 0; i < tope; i++)
+            {
+                if (i > 0)
+                {
+                    dato += "\n";
+                }
+                dato += $"[{i}]-{array[i]}";
+            }
+
+            return dato;
+        }
+    }
+}
---
+++ PilasColas/Program.cs
@@ -0,0 +1,60 @@
+﻿using Arreglos;
+using System;
+
+namespace PilasColas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ArrayPilas pila = new ArrayPilas(5);
+
+                pila.Agregar("cero");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Agregar("uno");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Agregar("dos");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Agregar("tres");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Agregar("cuatro");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Agregar("cinco");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("-----------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
